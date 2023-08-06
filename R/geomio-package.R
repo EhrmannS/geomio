b6468dd91cb5d80c013029b12c504586353b983a
EhrmannS <steffen.ehrmann@posteo.de>
2023-08-07 01:49:10 +0200

    update package

--- R/geomio-package.R
+++ R/geomio-package.R
@@ -1,6 +1,13 @@
 #' geomio: geometry input/output
 #'
 #' The geomio package ...
+#' geomio fills this gap by providing tools
+#' \itemize{
+#'   \item that produce an identical output for the same metadata of different
+#'         classes (via so-called getters) and
+#'   \item that use an identical input to write to various classes that
+#'         originally require different input (via so-called setters).
+#' }
 #'
 #' @author \strong{Maintainer, Author}: Steffen Ehrmann
 #'   \email{steffen.ehrmann@posteo.de}
